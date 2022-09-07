9e1d2073851a92f8c9a6975d327bdb431a71cb7e
Luís Felipe Cruz <lfscruz2001@gmail.com>
2022-09-07 18:10:27 -0300

    post funcionando

--- script.js
+++ script.js
@@ -1,69 +1,95 @@
 let modelos = document.querySelectorAll('.modelo');
+let modeloClasses;
 let escolhaModelo;
 let golas = document.querySelectorAll('.gola');
+let golaClasses;
 let escolhaGola;
 let tecidos = document.querySelectorAll('.tecido');
+let tecidoClasses;
 let escolhaTecido;
-let botao = document.querySelector('button')
-let input = document.querySelector('input')
-let pedidos = document.querySelector('.ultimos-pedidos')
+let botao = document.querySelector('.botao');
+let input = document.querySelector('input');
+let pedidos = document.querySelector('.ultimos-pedidos');
 let pedidosAPI;
+let nome = prompt('Qual o seu nome?')
+let pedidoEnviado = {
+	model: escolhaModelo,
+	neck: escolhaGola,
+	material: escolhaTecido,
+	image: 'https://cdn.rushordertees.com/design/ZoomImage.php?src=NTUyMTM2Mw_f&style=RT2000&colorCode=WHT&x=240&y=300&width=880&height=880&scale=1.7&watermark=false&autoInvertDesign=true',
+	owner: nome,
+	author: nome
+}
 
-const promessa = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts')
-promessa.then(resposta)
+const promessa = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
+
+function carregarPedidos(){
+    promessa.then(resposta);
+
+};
+
+function mandarPedido(){  
+    pedidos.innerHTML = ''
+    if (ativarBotao){
+        // axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts', pedidoEnviado)
+        console.log('pode ir')
+    }
+    carregarPedidos()
+    console.log(pedidoEnviado)
+}
+
+carregarPedidos();
 
 function resposta(resposta){
-    console.log(resposta.data)
-    pedidosAPI = resposta.data
-    console.log(pedidosAPI)
-    ultimosPedidos()
-}
+    pedidosAPI = resposta.data;
+    ultimosPedidos();
+};
 
 function selecionarModelo(escolha){
     modelos.forEach(element => {
-        element.classList.remove('selecionado')
+        element.classList.remove('selecionado');
     });
-    escolhaModelo = escolha.parentNode.classList
-    escolhaModelo.add('selecionado')
-    console.log(escolhaModelo)
-    console.log(escolhaGola)
-    console.log(escolhaTecido)
+    escolhaModelo = escolha.id;
+    modeloClasses = escolha.parentNode.classList;
+    modeloClasses.add('selecionado');
     ativarBotao();
-}
+};
 
 function selecionarGola(escolha){
     golas.forEach(element => {
-        element.classList.remove('selecionado')
+        element.classList.remove('selecionado');
     })
-    escolhaGola = escolha.parentNode.classList
-    escolhaGola.add('selecionado')
-    console.log(escolhaModelo)
-    console.log(escolhaGola)
-    console.log(escolhaTecido)
-    ativarBotao();
+    escolhaGola = escolha.id;
+    golaClasses = escolha.parentNode.classList;
+    golaClasses.add('selecionado');
+    ativarBotao();    
 }
 
 function selecionarTecido(escolha){
     tecidos.forEach(element => {
-        element.classList.remove('selecionado')
+        element.classList.remove('selecionado');
     })
-    escolhaTecido = escolha.parentNode.classList
-    escolhaTecido.add('selecionado')
-    console.log(escolhaModelo)
-    console.log(escolhaGola)
-    console.log(escolhaTecido)
+    escolhaTecido = escolha.id;
+    tecidoClasses = escolha.parentNode.classList;
+    tecidoClasses.add('selecionado');
     ativarBotao();
+    
 }
 
 function ativarBotao(){
-    if (escolhaModelo[2] == "selecionado" && escolhaGola[2] == "selecionado" && escolhaTecido[2] == "selecionado"){
-        botao.classList.add('botao-ativado')
+    if (modeloClasses[2] == "selecionado" && golaClasses[2] == "selecionado" && tecidoClasses[2] == "selecionado"){
+        botao.innerHTML = `<button onclick="mandarPedido();">Confirmar pedido</button>`
+        botao.classList.add('botao-ativado');
+        console.log(botao)
+        pedidoEnviado.model = escolhaModelo;
+        pedidoEnviado.neck = escolhaGola;
+        pedidoEnviado.material = escolhaTecido;
+        console.log(pedidoEnviado)
     }
 }
 
 function ultimosPedidos(){
     for(let i = 0; i < pedidosAPI.length; i++){
-        console.log(pedidosAPI[i])
         pedidos.innerHTML += `
         <li>
             <img class="imagem-pedido" src="${pedidosAPI[i].image}" alt="Blusa1">
